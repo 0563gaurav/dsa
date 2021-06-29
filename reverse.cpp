3e7c54edfed5e6c509f6daaf9eef9ef264ef8e5d
Gaurav <hkrgs1234@gmail.com>
2021-06-28 22:36:22 -0400

    Initial Commit

---
+++ reverse.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#define size 7
+int* reverse_array(int array[]);
+
+using namespace std;
+int main(){
+	int arr[size];
+
+	for(int i=0;i<size;i++){
+		cout<<"Enter the "<<i<<"th element of array: ";
+		cin>>arr[i];
+	}
+	cout<<"Old Array Element:  \n";
+	for(int i=0;i<size;i++){
+		cout<<arr[i]<<endl;
+	}
+	cout<<"New Array: \n";
+	int *p ;
+	p=reverse_array(arr);
+	for(int i=0;i<size;i++){
+	cout<<p[i]<<endl;
+	}
+
+
+	return 0;
+}
+
+int*  reverse_array(int array[size]){
+	for(int i=0;i<(int)(size/2);i++){
+			int temp=0;
+			temp=array[i];
+			array[i]=array[size-(i+1)];
+			array[size-(i+1)]=temp;
+	}
+	return array;
+
+
+
+}
+		
